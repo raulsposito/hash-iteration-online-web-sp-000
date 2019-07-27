e632904a029883ca914271446c6230c4d296484a
Raul Sposito <raulsposito@gmail.com>
2019-07-27 22:58:57 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,6 +15,7 @@ def age_appropriate_birthday(birthday_kids)
     if age <= 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
-  else puts "You are too old for this"
+  else 
+    "You are too old for this"
   end
 end
