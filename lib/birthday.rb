30ff04942e1f615a6c049dd287e2463dbe9d7a72
Raul Sposito <raulsposito@gmail.com>
2019-07-27 22:51:45 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,10 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
+    if age <= 12 
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  else false 
+end
   end
 end
 
